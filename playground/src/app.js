b3d4e5bcf9842f8627a4599c3d9fa895f0634b1f
Nikola Boychev <nboychev90@gmail.com>
2020-04-28 15:24:30 +0300

    Nest bottomTabs > stack in another stack

--- playground/src/app.js
+++ playground/src/app.js
@@ -30,57 +30,63 @@ function start() {
 function setRoot() {
   Navigation.setRoot({
     root: {
-      bottomTabs: {
+      stack: {
         children: [
           {
-            stack: {
+            bottomTabs: {
               children: [
                 {
-                  component: {
-                    name: 'Layouts'
-                  }
-                }
-              ],
-              options: {
-                bottomTab: {
-                  text: 'Layouts',
-                  icon: require('../img/layouts.png'),
-                  selectedIcon: require('../img/layouts_selected.png'),
-                  testID: testIDs.LAYOUTS_TAB
-                }
-              }
-            }
-          },
-          {
-            stack: {
-              children: [
-                {
-                  component: {
-                    name: 'Options'
-                  }
-                }
-              ],
-              options: {
-                topBar: {
-                  title: {
-                    text: 'Default Title'
+                  stack: {
+                    children: [
+                      {
+                        component: {
+                          name: 'Layouts'
+                        }
+                      }
+                    ],
+                    options: {
+                      bottomTab: {
+                        text: 'Layouts',
+                        icon: require('../img/layouts.png'),
+                        selectedIcon: require('../img/layouts_selected.png'),
+                        testID: testIDs.LAYOUTS_TAB
+                      }
+                    }
                   }
                 },
-                bottomTab: {
-                  text: 'Options',
-                  icon: require('../img/options.png'),
-                  selectedIcon: require('../img/options_selected.png'),
-                  testID: testIDs.OPTIONS_TAB
-                }
-              }
-            }
-          },
-          {
-            stack: {
-              children: [
                 {
-                  component: {
-                    name: 'Navigation'
+                  stack: {
+                    children: [
+                      {
+                        component: {
+                          name: 'Options'
+                        }
+                      }
+                    ],
+                    options: {
+                      topBar: {
+                        title: {
+                          text: 'Default Title'
+                        }
+                      },
+                      bottomTab: {
+                        text: 'Options',
+                        icon: require('../img/options.png'),
+                        selectedIcon: require('../img/options_selected.png'),
+                        testID: testIDs.OPTIONS_TAB
+                      }
+                    }
+                  }
+                },
+                {
+                  stack: {
+                    children: [
+                      {
+                        component: {
+                          name: 'Navigation'
+                        }
+                      }
+                    ]
                   }
                 }
               ]
